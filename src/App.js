074e337844c8d6393fe993fe58858f5a41d9c08f
user <kdhsam0715@naver.com>
2022-11-12 16:44:09 +0900

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/input";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import {Routes, Route, Link} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <nav>
+        <Link to = "/">Home</Link> | <Link to = "/about">About</Link> |{" "}
+        <Link to = "/counter">Counter</Link> | <Link to = "/input">Input</Link>
+
+      </nav>
+      <Routes>
+        <Route path = "/" element={<Home />} />
+        <Route path = "/about" element={<About />} />
+        <Route path = "/counter" element={<Counter />} />
+        <Route path = "/input" element={<Input />} />
+      </Routes>
+  */}
+      <NavBar />
+      <Banner />
+      {/* <Skills />
+      <Projects />
+      <Contact />
+      <Footer /> */} 
     </div>
   );
 }
---
+++ src/pages/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+    return <h1>어바웃 화면</h1>
+} 
+
+export default About;
